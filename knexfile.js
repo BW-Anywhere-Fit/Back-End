56be36dc43c902ec4ccd269c85902edd4d6499d2
Billy Baney <billybaney1@gmail.com>
2020-01-08 11:27:16 -0600

    fixing knex issue

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,23 @@
 
 // Update with your config settings.
-
+const dbConnection = process.env.DATABASE_URL
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
       filename: "./data/fitness.db3"
     },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "sqlite3",
+    connection:  dbConnection,
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
