bd17a790953234706d216913021e272cec979666
Billy Baney <billybaney1@gmail.com>
2020-01-09 18:14:38 -0600

    added tests

---
+++ server/server.spec.js
@@ -0,0 +1,71 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server.js', function() {
+
+    describe('GET /', function() {
+        
+        it ('should return a 200 OK', function() {
+            return request(server)
+                .get('/')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                });
+        });
+
+        it ('should return Welcome to the Anywhere Fitness API 💪', function() {
+            return request(server)
+                .get('/')
+                .then(res => {
+                    expect(res.body.message).toBe('Welcome to the Anywhere Fitness API 💪')
+                });
+        });
+    
+    });
+
+    describe('/auth/register', function() {
+
+        it ('should return status code 201', function() {
+            return request(server)
+                .post('/api/auth/register')
+                .send({ 
+                    username: `${Date.now()}`, 
+                    password: `${Date.now()}`, 
+                    role_id: 2})
+                .expect(201)
+        });
+
+        it ('is a json object', function() {
+            return request(server)
+                .post('/api/auth/register')
+                .send({ username: 'instructor3', password: 'pass123'})
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                });
+        });
+
+    }); // end register
+    
+    describe('/auth/login', function() {
+
+        it ('should return a 200 OK', function() {
+            return request(server)
+                .post('/api/auth/login')
+                .send({ username: 'instructor3', password: 'pass123'})
+                .then(res => {
+                    expect(res.status).toBe(200)
+                });
+        });
+        
+        it ('is a json object', function() {
+            return request(server)
+                .post('/api/auth/login')
+                .send({ username: 'instructor3', password: 'pass123'})
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                });
+        });
+
+    });
+
+});
